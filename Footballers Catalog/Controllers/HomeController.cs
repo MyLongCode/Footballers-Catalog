8592b45d4b433e1cbbf15f4fb16769b7e53bf0cd
mylongcode <mylongcode@gmail.com>
2024-01-29 21:16:29 +0300

    Add project files.

---
+++ Footballers Catalog/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Footballers_Catalog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Footballers_Catalog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
