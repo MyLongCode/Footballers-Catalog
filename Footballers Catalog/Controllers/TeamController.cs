e2f98ac5021cd03fd03b0aa9d735c9ce0d8283b9
mylongcode <mylongcode@gmail.com>
2024-01-31 13:40:15 +0300

    add team controller

---
+++ Footballers Catalog/Controllers/TeamController.cs
@@ -0,0 +1,18 @@
+﻿using Footballers_Catalog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Footballers_Catalog.Controllers
+{
+    public class TeamController : Controller
+    {
+        ApplicationContext db;
+        public TeamController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
